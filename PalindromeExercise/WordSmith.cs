8f0185d4d182ce4496c26d042f0ff283369cf288
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-25 16:56:33 -0400

    done

---
+++ PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            if (word.ToLower()=="palindrome")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
+
---
+++ PalindromeExerciseTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using PalindromeExercise;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("palindrome",true)]
+        [InlineData("paLindroMe", true)]
+        [InlineData("Palindrome", true)]
+        [InlineData("random", false)]
+        [InlineData("1398", false)]
+        public void Test1(string word, bool expected)
+        {
+            var wordSmit = new WordSmith();
+
+        var actual = wordSmit.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
+
+
+
+
+
+    }
+    }
+}
